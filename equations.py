8cf9e4936672a70da25c0a3ca90fc032b243c88a
noamt2021 <taichman2@gmail.com>
2021-03-19 13:24:28 +0200

    second

--- equations.py
+++ equations.py
@@ -13,12 +13,13 @@ def exponent (x):
         e=e+temp/a
         i=i+1
     return e
+print(exponent(x))
     
 def Ln (x):
     if x==0:
         return 0
-    if x<=0:
-        return 0
+    if x<0:
+        x= x*-1
     y= 1
     i= 0
     
@@ -26,6 +27,7 @@ def Ln (x):
         y=y+2*((x-exponent(y))/(x+exponent(y)))  
         i=i+1
     return y
+print (Ln (x))
 
 def XtimesY (x,y):
     if x==0:
@@ -33,18 +35,20 @@ def XtimesY (x,y):
     Hezka = exponent(y*Ln(x))
     return Hezka
 
+print (XtimesY(x, y))
 #B
 def sqrt (x,y):
     if (y<=0) and (x%2==0):
         return 0
     b = exponent((1/x)*(Ln (y)))
     return b
+print (sqrt(x, y))
 
 #C
 def calculate (x):
     calculator = exponent(x)*XtimesY(7,x)*(1/x)*sqrt(x,x) 
     return calculator
-
+print (calculate(x))
     
         
     
